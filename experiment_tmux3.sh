4dff43774045f44bab34f4a94888272442c43ba9
Haoran Zhang <haoranz5@andrew.cmu.edu>
2023-12-24 08:16:08 -0500

    update main and parser to make it only train 1 algo. plotAll.py needs to change in the future.

---
+++ experiment_tmux3.sh
@@ -0,0 +1,13 @@
+#!/bin/bash
+python main.py --exp_num 1 --iid_type noniid noniid noniid iid iid --class_ratio 0.35 --notes bayes_decay0.9Pa8 --bayes_decay 0.9 --powerfulCNN --alpha 8
+python main.py --exp_num 1 --iid_type noniid noniid noniid iid iid --class_ratio 0.35 --notes bayes_decay0.9Pa9 --bayes_decay 0.9 --powerfulCNN --alpha 9
+python main.py --exp_num 1 --iid_type noniid noniid noniid iid iid --class_ratio 0.35 --notes bayes_decay0.9Pa10 --bayes_decay 0.9 --powerfulCNN --alpha 10
+python main.py --exp_num 1 --iid_type iid iid iid iid iid --class_ratio 0.35 --notes bayes_decay0.9Pa8 --bayes_decay 0.9 --powerfulCNN --alpha 8
+python main.py --exp_num 1 --iid_type iid iid iid iid iid --class_ratio 0.35 --notes bayes_decay0.9Pa9 --bayes_decay 0.9 --powerfulCNN --alpha 9
+python main.py --exp_num 1 --iid_type iid iid iid iid iid --class_ratio 0.35 --notes bayes_decay0.9Pa10 --bayes_decay 0.9 --powerfulCNN --alpha 10
+python main.py --exp_num 1 --iid_type iid iid iid iid iid --class_ratio 0.35 --notes bayes_decay0.9a8 --bayes_decay 0.9 --alpha 8
+python main.py --exp_num 1 --iid_type iid iid iid iid iid --class_ratio 0.35 --notes bayes_decay0.9a9 --bayes_decay 0.9 --alpha 9
+python main.py --exp_num 1 --iid_type iid iid iid iid iid --class_ratio 0.35 --notes bayes_decay0.9a10 --bayes_decay 0.9 --alpha 10
+python main.py --exp_num 1 --iid_type noniid noniid noniid iid iid --class_ratio 0.35 --notes bayes_decay0.9a8 --bayes_decay 0.9 --alpha 8
+python main.py --exp_num 1 --iid_type noniid noniid noniid iid iid --class_ratio 0.35 --notes bayes_decay0.9a9 --bayes_decay 0.9 --alpha 9
+python main.py --exp_num 1 --iid_type noniid noniid noniid iid iid --class_ratio 0.35 --notes bayes_decay0.9a10 --bayes_decay 0.9 --alpha 10
