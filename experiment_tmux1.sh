977b7acdf9ef252b9829176373b45663caad016b
Haoran Zhang <haoranz5@andrew.cmu.edu>
2024-01-28 17:16:01 -0500

    test noniid 5tasks bayesian

--- experiment_tmux1.sh
+++ experiment_tmux1.sh
@@ -1,23 +1,24 @@
 #!/bin/bash
-# exp0
-python main.py --exp_num 1 --C 1.0 --num_clients 20 --iid_type iid iid iid iid iid --task_type mnist cifar10 fashion_mnist emnist cifar10 --notes exp3C1c20d1.5-cpu-seed15 --cpumodel --seed 15 --data_ratio 1.5
-python main.py --exp_num 1 --C 1.0 --num_clients 20 --iid_type iid iid iid iid iid --task_type mnist cifar10 fashion_mnist emnist cifar10 --notes exp3C1c20d1.5-cpu-seed12 --cpumodel --seed 12 --data_ratio 1.5
-python main.py --exp_num 1 --C 1.0 --num_clients 20 --iid_type iid iid iid iid iid --task_type mnist cifar10 fashion_mnist emnist cifar10 --notes exp3C1c20d1.5-cpu-seed11 --cpumodel --seed 11 --data_ratio 1.5
-python main.py --exp_num 1 --C 1.0 --num_clients 20 --iid_type iid iid iid iid iid --task_type mnist cifar10 fashion_mnist emnist cifar10 --notes exp3C1c20d2.0-cpu-seed15 --cpumodel --seed 15 --data_ratio 2.0
-python main.py --exp_num 1 --C 1.0 --num_clients 20 --iid_type iid iid iid iid iid --task_type mnist cifar10 fashion_mnist emnist cifar10 --notes exp3C1c20d2.0-cpu-seed12 --cpumodel --seed 12 --data_ratio 2.0
-python main.py --exp_num 1 --C 1.0 --num_clients 20 --iid_type iid iid iid iid iid --task_type mnist cifar10 fashion_mnist emnist cifar10 --notes exp3C1c20d2.0-cpu-seed11 --cpumodel --seed 11 --data_ratio 2.0
-python main.py --exp_num 1 --C 1.0 --num_clients 20 --iid_type iid iid iid iid iid --task_type mnist cifar10 fashion_mnist emnist cifar10 --notes exp3C1c20d2.5-cpu-seed15 --cpumodel --seed 15 --data_ratio 2.5
-python main.py --exp_num 1 --C 1.0 --num_clients 20 --iid_type iid iid iid iid iid --task_type mnist cifar10 fashion_mnist emnist cifar10 --notes exp3C1c20d2.5-cpu-seed12 --cpumodel --seed 12 --data_ratio 2.5
-python main.py --exp_num 1 --C 1.0 --num_clients 20 --iid_type iid iid iid iid iid --task_type mnist cifar10 fashion_mnist emnist cifar10 --notes exp3C1c20d2.5-cpu-seed11 --cpumodel --seed 11 --data_ratio 2.5
+# test bayesian in noniid
+# try to avoid poor training (take out mnist)
+python main.py --exp_num 1 --C 1.0 --num_clients 20 --class_ratio 0.3 0.3 0.3 0.3 0.3 --iid_type noniid noniid noniid noniid noniid --task_type cifar10 cifar10 fashion_mnist emnist cifar10 --notes exp3C1c20d0.5-seed15 --cpumodel --seed 15 --data_ratio 0.5
+python main.py --exp_num 1 --C 1.0 --num_clients 20 --class_ratio 0.3 0.3 0.3 0.3 0.3 --iid_type noniid noniid noniid noniid noniid --task_type cifar10 cifar10 fashion_mnist emnist cifar10 --notes exp3C1c20d1.5-seed15 --cpumodel --seed 15 --data_ratio 1.5
+python main.py --exp_num 1 --C 1.0 --num_clients 20 --class_ratio 0.3 0.3 0.3 0.3 0.3 --iid_type noniid noniid noniid noniid noniid --task_type cifar10 cifar10 fashion_mnist emnist cifar10 --notes exp3C1c20d1.0-seed15 --cpumodel --seed 15 --data_ratio 1.0
+python main.py --exp_num 1 --C 1.0 --num_clients 20 --class_ratio 0.3 0.3 0.3 0.3 0.3 --iid_type noniid noniid noniid noniid noniid --task_type cifar10 cifar10 fashion_mnist emnist cifar10 --notes exp3C1c20d2.0-seed15 --cpumodel --seed 15 --data_ratio 2.0
+# try different class ratio
+# try less tasks
+python main.py --exp_num 1 --C 1.0 --num_clients 20 --class_ratio 0.3 0.3 0.3 --iid_type noniid noniid noniid --task_type cifar10 fashion_mnist emnist --notes exp3C1c20d0.5-seed15 --cpumodel --seed 15 --data_ratio 0.5
+python main.py --exp_num 1 --C 1.0 --num_clients 20 --class_ratio 0.3 0.3 0.3 --iid_type noniid noniid noniid --task_type cifar10 fashion_mnist emnist --notes exp3C1c20d1.5-seed15 --cpumodel --seed 15 --data_ratio 1.5
+python main.py --exp_num 1 --C 1.0 --num_clients 20 --class_ratio 0.3 0.3 0.3 --iid_type noniid noniid noniid --task_type cifar10 fashion_mnist emnist --notes exp3C1c20d1.0-seed15 --cpumodel --seed 15 --data_ratio 1.0
+python main.py --exp_num 1 --C 1.0 --num_clients 20 --class_ratio 0.3 0.3 0.3 --iid_type noniid noniid noniid --task_type cifar10 fashion_mnist emnist --notes exp3C1c20d2.0-seed15 --cpumodel --seed 15 --data_ratio 2.0
 
-
-
-#python main.py --exp_num 1 --C 0.25 --num_clients 80 --data_ratio 0.5 --algo_type proposed random round_robin --iid_type iid iid iid iid iid --task_type mnist cifar10 fashion_mnist emnist cifar10 --notes exp2.5C0.25c80-cpu-d0.5-seed15 --cpumodel --seed 15
-#python main.py --exp_num 1 --C 0.25 --num_clients 120 --data_ratio 0.5 --algo_type proposed random round_robin --iid_type iid iid iid iid iid --task_type mnist cifar10 fashion_mnist emnist cifar10 --notes exp2.5C0.25c120-cpu-d0.5-seed15 --cpumodel --seed 15
-#python main.py --exp_num 1 --C 0.25 --num_clients 160 --data_ratio 0.5 --algo_type proposed random round_robin --iid_type iid iid iid iid iid --task_type mnist cifar10 fashion_mnist emnist cifar10 --notes exp2.5C0.25c160-cpu-d0.5-seed15 --cpumodel --seed 15
-#python main.py --exp_num 1 --C 0.25 --num_clients 80 --data_ratio 0.5 --algo_type proposed random round_robin --iid_type iid iid iid iid iid --task_type mnist cifar10 fashion_mnist emnist cifar10 --notes exp2.5C0.25c80-cpu-d0.5-seed16 --cpumodel --seed 16
-#python main.py --exp_num 1 --C 0.25 --num_clients 120 --data_ratio 0.5 --algo_type proposed random round_robin --iid_type iid iid iid iid iid --task_type mnist cifar10 fashion_mnist emnist cifar10 --notes exp2.5C0.25c120-cpu-d0.5-seed16 --cpumodel --seed 16
-#python main.py --exp_num 1 --C 0.25 --num_clients 160 --data_ratio 0.5 --algo_type proposed random round_robin --iid_type iid iid iid iid iid --task_type mnist cifar10 fashion_mnist emnist cifar10 --notes exp2.5C0.25c160-cpu-d0.5-seed16 --cpumodel --seed 16
-#python main.py --exp_num 1 --C 0.25 --num_clients 80 --data_ratio 0.5 --algo_type proposed random round_robin --iid_type iid iid iid iid iid --task_type mnist cifar10 fashion_mnist emnist cifar10 --notes exp2.5C0.25c80-cpu-d0.5-seed17 --cpumodel --seed 17
-#python main.py --exp_num 1 --C 0.25 --num_clients 120 --data_ratio 0.5 --algo_type proposed random round_robin --iid_type iid iid iid iid iid --task_type mnist cifar10 fashion_mnist emnist cifar10 --notes exp2.5C0.25c120-cpu-d0.5-seed17 --cpumodel --seed 17
-#python main.py --exp_num 1 --C 0.25 --num_clients 160 --data_ratio 0.5 --algo_type proposed random round_robin --iid_type iid iid iid iid iid --task_type mnist cifar10 fashion_mnist emnist cifar10 --notes exp2.5C0.25c160-cpu-d0.5-seed17 --cpumodel --seed 17
+python main.py --exp_num 1 --C 1.0 --num_clients 20 --class_ratio 0.3 0.3 0.3 0.3 0.3 --iid_type noniid noniid noniid noniid noniid --task_type cifar10 cifar10 fashion_mnist emnist cifar10 --notes exp3C1c20d0.5-seed10 --cpumodel --seed 10 --data_ratio 0.5
+python main.py --exp_num 1 --C 1.0 --num_clients 20 --class_ratio 0.3 0.3 0.3 0.3 0.3 --iid_type noniid noniid noniid noniid noniid --task_type cifar10 cifar10 fashion_mnist emnist cifar10 --notes exp3C1c20d1.5-seed10 --cpumodel --seed 10 --data_ratio 1.5
+python main.py --exp_num 1 --C 1.0 --num_clients 20 --class_ratio 0.3 0.3 0.3 0.3 0.3 --iid_type noniid noniid noniid noniid noniid --task_type cifar10 cifar10 fashion_mnist emnist cifar10 --notes exp3C1c20d1.0-seed10 --cpumodel --seed 10 --data_ratio 1.0
+python main.py --exp_num 1 --C 1.0 --num_clients 20 --class_ratio 0.3 0.3 0.3 0.3 0.3 --iid_type noniid noniid noniid noniid noniid --task_type cifar10 cifar10 fashion_mnist emnist cifar10 --notes exp3C1c20d2.0-seed10 --cpumodel --seed 10 --data_ratio 2.0
+# try different class ratio
+# try less tasks
+python main.py --exp_num 1 --C 1.0 --num_clients 20 --class_ratio 0.3 0.3 0.3 --iid_type noniid noniid noniid --task_type cifar10 fashion_mnist emnist --notes exp3C1c20d0.5-seed10 --cpumodel --seed 10 --data_ratio 0.5
+python main.py --exp_num 1 --C 1.0 --num_clients 20 --class_ratio 0.3 0.3 0.3 --iid_type noniid noniid noniid --task_type cifar10 fashion_mnist emnist --notes exp3C1c20d1.5-seed10 --cpumodel --seed 10 --data_ratio 1.5
+python main.py --exp_num 1 --C 1.0 --num_clients 20 --class_ratio 0.3 0.3 0.3 --iid_type noniid noniid noniid --task_type cifar10 fashion_mnist emnist --notes exp3C1c20d1.0-seed10 --cpumodel --seed 10 --data_ratio 1.0
+python main.py --exp_num 1 --C 1.0 --num_clients 20 --class_ratio 0.3 0.3 0.3 --iid_type noniid noniid noniid --task_type cifar10 fashion_mnist emnist --notes exp3C1c20d2.0-seed10 --cpumodel --seed 10 --data_ratio 2.0
