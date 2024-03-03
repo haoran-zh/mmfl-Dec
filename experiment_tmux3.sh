1472b46f2e9f765c7c41db0f92789a9cba4361a4
Haoran Zhang <haoranz5@andrew.cmu.edu>
2024-03-02 20:53:04 -0500

    example command to test all algorithms.

--- experiment_tmux3.sh
+++ experiment_tmux3.sh
@@ -1,27 +1,13 @@
 #!/bin/bash
 # alpha-fair with ms optimal sampling
-lr_list=(100)
-for lr in "${lr_list[@]}"; do
-  python main.py --L $lr --fairness notfair --unbalance 0.9 0.1 --alpha 3 --notes 0.01unbalance_ms_a3_lr$lr --optimal_sampling --C 0.2 --num_clients 60 --class_ratio 0.5 0.5 0.5 --iid_type noniid noniid noniid --task_type fashion_mnist mnist mnist --algo_type proposed --seed 15 --cpumodel --local_epochs 1 1 1 --round_num 300 --insist
-done
-# alpha-fair without optimal sampling
-lr_list=(100)
-for lr in "${lr_list[@]}"; do
-  python main.py --L $lr --fairness notfair --unbalance 0.9 0.1 --alpha 3 --notes 0.01unbalance_a3_lr$lr --C 0.2 --num_clients 60 --class_ratio 0.5 0.5 0.5 --iid_type noniid noniid noniid --task_type fashion_mnist mnist mnist --algo_type proposed --seed 15 --cpumodel --local_epochs 1 1 1 --round_num 300 --insist
-done
-# random
-lr_list=(100)
-for lr in "${lr_list[@]}"; do
-  python main.py --L $lr --fairness notfair --unbalance 0.9 0.1 --alpha 3 --notes 0.01unbalance_random_lr$lr --C 0.2 --num_clients 60 --class_ratio 0.5 0.5 0.5 --iid_type noniid noniid noniid --task_type fashion_mnist mnist mnist --algo_type random --seed 15 --cpumodel --local_epochs 1 1 1 --round_num 300 --insist
-done
-
-# taskfair
-lr_list=(100)
-for lr in "${lr_list[@]}"; do
-  python main.py --L $lr --fairness taskfair --unbalance 0.9 0.1 --alpha 3 --notes 0.01unbalance_OS_taskfair_a3_lr$lr --alpha_loss --optimal_sampling --C 0.2 --num_clients 60 --class_ratio 0.5 0.5 0.5 --iid_type noniid noniid noniid --task_type fashion_mnist mnist mnist --algo_type proposed --seed 15 --cpumodel --local_epochs 1 1 1 --round_num 1500 --insist
-done
-# clientfair
-lr_list=(100)
-for lr in "${lr_list[@]}"; do
-  python main.py --L $lr --fairness clientfair --unbalance 0.9 0.1 --alpha 3 --notes 0.01unbalance_OS_clientfair_a3_lr$lr --alpha_loss --optimal_sampling --C 0.2 --num_clients 60 --class_ratio 0.5 0.5 0.5 --iid_type noniid noniid noniid --task_type fashion_mnist mnist mnist --algo_type proposed --seed 15 --cpumodel --local_epochs 1 1 1 --round_num 1500 --insist
+seedlist=(11 12 13 14)
+for sd in "${seedlist[@]}"; do
+  python main.py --L 100 --fairness notfair --unbalance 0.9 0.1 --alpha 1.5 --notes 0.01u_msAS_a1.5_$sd --approx_optimal --C 0.2 --num_clients 40 --class_ratio 0.3 0.3 0.3 --iid_type noniid noniid noniid --task_type fashion_mnist mnist --algo_type proposed --seed $sd --cpumodel --local_epochs 1 1 1 --round_num 300 --insist
+  python main.py --L 100 --fairness notfair --unbalance 0.9 0.1 --alpha 1.5 --notes 0.01u_random_$sd --C 0.2 --num_clients 40 --class_ratio 0.3 0.3 0.3 --iid_type noniid noniid noniid --task_type fashion_mnist mnist --algo_type random --seed $sd --cpumodel --local_epochs 1 1 1 --round_num 300 --insist
+  python main.py --L 100 --fairness notfair --unbalance 0.9 0.1 --alpha 1.5 --notes 0.01u_ms_a1.5_$sd --optimal_sampling --C 0.2 --num_clients 40 --class_ratio 0.3 0.3 0.3 --iid_type noniid noniid noniid --task_type fashion_mnist mnist --algo_type proposed --seed $sd --cpumodel --local_epochs 1 1 1 --round_num 300 --insist
+  python main.py --L 100 --fairness notfair --unbalance 0.9 0.1 --alpha 1.5 --notes 0.01u_a1.5_$sd --C 0.2 --num_clients 40 --class_ratio 0.3 0.3 0.3 --iid_type noniid noniid noniid --task_type fashion_mnist mnist --algo_type proposed --seed $sd --cpumodel --local_epochs 1 1 1 --round_num 300 --insist
+  python main.py --L 100 --fairness taskfair --unbalance 0.9 0.1 --alpha 1.5 --notes 0.01u_OS_taskfair_a1.5_$sd --alpha_loss --optimal_sampling --C 0.2 --num_clients 40 --class_ratio 0.3 0.3 0.3 --iid_type noniid noniid noniid --task_type fashion_mnist mnist --algo_type proposed --seed $sd --cpumodel --local_epochs 1 1 1 --round_num 1500 --insist
+  python main.py --L 100 --fairness clientfair --unbalance 0.9 0.1 --alpha 1.5 --notes 0.01u_OS_clientfair_a1.5_$sd --alpha_loss --optimal_sampling --C 0.2 --num_clients 40 --class_ratio 0.3 0.3 0.3 --iid_type noniid noniid noniid --task_type fashion_mnist mnist --algo_type proposed --seed $sd --cpumodel --local_epochs 1 1 1 --round_num 1500 --insist
+  python main.py --L 100 --fairness taskfair --unbalance 0.9 0.1 --alpha 1.5 --notes 0.01u_AS_taskfair_a1.5_$sd --alpha_loss --approx_optimal --C 0.2 --num_clients 40 --class_ratio 0.3 0.3 0.3 --iid_type noniid noniid noniid --task_type fashion_mnist mnist --algo_type proposed --seed $sd --cpumodel --local_epochs 1 1 1 --round_num 1500 --insist
+  python main.py --L 100 --fairness clientfair --unbalance 0.9 0.1 --alpha 1.5 --notes 0.01u_AS_clientfair_a1.5_$sd --alpha_loss --approx_optimal --C 0.2 --num_clients 40 --class_ratio 0.3 0.3 0.3 --iid_type noniid noniid noniid --task_type fashion_mnist mnist --algo_type proposed --seed $sd --cpumodel --local_epochs 1 1 1 --round_num 1500 --insist
 done
