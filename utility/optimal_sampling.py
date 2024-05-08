8901e20cdda5abe1cdbbf5cc6fc2c8abe5c223af
Haoran Zhang <haoranz5@andrew.cmu.edu>
2024-05-08 10:24:42 -0400

    add a suboptimal way

--- utility/optimal_sampling.py
+++ utility/optimal_sampling.py
@@ -36,47 +36,75 @@ def power_gradient_norm(gradient_norm, tasks_local_training_loss, args, all_data
     return gradient_norm_power
 
 
-def optimal_sampling_giventask(sample_num, all_data_num, gradient_record, task_index, group_client): # gradient record is norm
-    # get the task idx, and current group of clients available to this task
-    # do the optimal sampling only on this task (so it is a single task sampling in fact)
-    # m in the paper
-    # input-----
-    # sample_num: the number of expected active clients in total
-    # all_data_num: the number of data for each client
-    # gradient_record: U_{i,s} in the paper. It can be gradient, or loss value.
-    # task_index: the index of the current task
-    # group_clients: the group of clients used for this task
-    # output-----
-    # chosen_client: active clients for this round.
-    # clients_task: the task of each client.
-    # p_dict: the probability of each client to be chosen for its task.
-    # using group_sampling need fewer clients to run their current loss.
-    tasks_num = 1
+def get_optimal_sampling_single(m, gradient_record): # gradient record is norm
+    # gradient_record: the shape is [task_index][client_index]
+    # chosen_clients provide the index of the chosen clients in a random order
+    # clients_task has the same order as chosen_clients
+    # multiple tasks sampling will degenerate to single task sampling when task=1
+    # therefore we can use the same function.
+    sample_num = m  # m in the paper
     # random.shuffle(task_indices) # make task order random
-    all_clients_num = len(group_client)
+    all_clients_num = len(gradient_record)
+
     all_gradients = gradient_record.copy()
-    dis = np.zeros(all_clients_num)
-    for i in range(all_clients_num):
-        dis[i] = all_data_num[task_index][group_client[i]]
-    dis = dis / np.sum(dis)
-    for i in range(all_clients_num):
-        # from U to U~ in the paper
-        all_gradients[task_index][group_client[i]] *= dis[i]
-
-    client_gradients_sumTasks = np.zeros(all_clients_num)  # this is M_i in the proof
-    #for client_index in range(all_clients_num):
-    for i, client_index in enumerate(group_client):
-        client_gradients_sumTasks[i] += all_gradients[task_index][client_index]
-
     # sort the gradients of the clients for this task, get a list of indices
-    sorted_indices = np.argsort(client_gradients_sumTasks)
+    sorted_indices = np.argsort(all_gradients)
 
     n = all_clients_num
     m = sample_num
 
     l = n - m + 1
     best_l = l
-    if m == 0:  # if m=0, we get best_l = n+1 above, which is wrong. how to solve?
+    if m == 0: # if m=0, we get best_l = n+1 above, which is wrong. how to solve?
+        best_l = n
+
+    while True:
+        l += 1
+        if l > n:
+            break
+        # sum the first l smallest gradients
+        sum_upto_l = sum(all_gradients[sorted_indices[i]] for i in range(l))
+        upper = sum_upto_l / all_gradients[sorted_indices[l-1]]
+        # if 0<m+l-n<=upper, then this l is good. find the largest l satisfying this condition
+        if 0 < m + l - n <= upper:
+            best_l = l
+    # compute p
+    p_i = np.zeros(all_clients_num)
+    sum_upto_l = sum(all_gradients[sorted_indices[i]] for i in range(best_l))
+    # print('sum_upto_l', sum_upto_l)
+    for i in range(len(sorted_indices)):
+        if i >= best_l:
+            p_i[sorted_indices[i]] = 1
+        else:
+            p_i[sorted_indices[i]] = (m + best_l - n) * all_gradients[sorted_indices[i]] / sum_upto_l
+
+    return p_i
+
+
+def get_optimal_sampling_tasks(m, gradient_record): # gradient record is norm
+    sample_num = int(m)  # m in the paper
+    tasks_num = len(gradient_record)
+    # random.shuffle(task_indices) # make task order random
+    all_clients_num = len(gradient_record[0])
+
+    all_gradients = gradient_record.copy()
+
+    client_gradients_sumTasks = np.zeros(all_clients_num) # this is M_i in the proof
+    for client_index in range(all_clients_num):
+        for task_index in range(tasks_num):
+            client_gradients_sumTasks[client_index] += all_gradients[task_index][client_index]
+
+    # sort the gradients of the clients for this task, get a list of indices
+    sorted_indices = np.argsort(client_gradients_sumTasks)
+    print(sorted_indices)
+
+    n = all_clients_num
+    m = sample_num
+
+    l = n - m + 1
+    print(l)
+    best_l = l
+    if m == 0: # if m=0, we get best_l = n+1 above, which is wrong. how to solve?
         best_l = n
 
     while True:
@@ -85,63 +113,136 @@ def optimal_sampling_giventask(sample_num, all_data_num, gradient_record, task_i
             break
         # sum the first l smallest gradients
         sum_upto_l = sum(client_gradients_sumTasks[sorted_indices[i]] for i in range(l))
-        upper = sum_upto_l / client_gradients_sumTasks[sorted_indices[l - 1]]
+        upper = sum_upto_l / client_gradients_sumTasks[sorted_indices[l-1]]
         # if 0<m+l-n<=upper, then this l is good. find the largest l satisfying this condition
         if 0 < m + l - n <= upper:
             best_l = l
     # compute p
-    p_s_i = np.zeros(all_clients_num)
+    p_s_i = np.zeros((tasks_num, all_clients_num))
     sum_upto_l = sum(client_gradients_sumTasks[sorted_indices[i]] for i in range(best_l))
     # print('sum_upto_l', sum_upto_l)
     for i in range(len(sorted_indices)):
         if i >= best_l:
-            p_s_i[sorted_indices[i]] = 1.0
+            for task_index in range(tasks_num):
+                p_s_i[task_index][sorted_indices[i]] = all_gradients[task_index][sorted_indices[i]] / client_gradients_sumTasks[sorted_indices[i]]
         else:
-            p_s_i[sorted_indices[i]] = (m + best_l - n) * client_gradients_sumTasks[
-                sorted_indices[i]] / sum_upto_l
-
-    allocation_result = np.zeros(all_clients_num, dtype=int)
-    for client_idx in range(all_clients_num):
-        if abs(1 - np.sum(p_s_i[client_idx])) < 1e-6:
-            p_not_choose = 0
-        else:
-            p_not_choose = 1 - p_s_i[client_idx]
-        # append p_not_choose to the head of p_s_i
-        p_client = np.zeros(tasks_num + 1)
-        p_client[0] = p_not_choose
-        p_client[1] = p_s_i[client_idx]
-        allocation_result[client_idx] = np.random.choice(np.arange(-1, tasks_num), p=p_client)
-    allocation_result = allocation_result.tolist()
-    clients_task = [task_index for s in allocation_result if s != -1]
-    chosen_clients = [group_client[i] for i in range(len(allocation_result)) if allocation_result[i] != -1]
-    # get p_dict
-    p_dict = [p_s_i[i] for i in range(all_clients_num) if allocation_result[i] == 0]
-    return clients_task, p_dict, chosen_clients
+            for task_index in range(tasks_num):
+                p_s_i[task_index][sorted_indices[i]] = (m + best_l - n) * all_gradients[task_index][sorted_indices[i]] / sum_upto_l
+    return p_s_i
 
 
-def evaluate_optimal_sampling(chosen_clients, all_data_num, gradient_record, global_result, alpha): # gradient record is norm
-    import itertools
-    sample_num = len(chosen_clients)  # m in the paper
-    tasks_num = len(gradient_record)
-    tasks_indices = np.arange(tasks_num)
-    # all possible subsets of tasks
-    max_subset_size = sample_num
-    all_tasks_subsets = []
-    for i in range(1, max_subset_size+1):
-        all_tasks_subsets.extend(list(itertools.combinations(tasks_indices, i)))
-    # print(all_tasks_subsets)
-    best_task_set_value = 0
-    for task_subset in all_tasks_subsets:
-        r, clients_task, p_dict, chosen_clients = optimal_sampling_giventask(sample_num, all_data_num, gradient_record, task_subset, global_result, alpha)
-        if r > best_task_set_value:
-            best_task_set_value = r
-            beset_task_set = task_subset
-            clients_task = clients_task
-            p_dict = p_dict
-            chosen_clients = chosen_clients
-    print(beset_task_set)
-    return clients_task, p_dict, chosen_clients
+def B_neq_Prob(gradients_i, ki, task_num):
+    p_i = get_optimal_sampling_single(m=ki, gradient_record=gradients_i)
+    return p_i
 
+def update_matrices(client_num, task_num, ki, active_num, all_gradients, set_matrix, p_si_matrix, ineq_matrix, evaluation_matrix):
+    for i in range(client_num):
+        for s in range(task_num):
+            if set_matrix[s, i] == 1:
+                # if set_matrix[:, i] all are zero, means it is in B_neq set
+                B_neq_sum = 0
+                Psi1_sum = 0
+                for j in range(client_num):
+                    if sum(set_matrix[:, j]) == 0:
+                        B_neq_sum += ki[j]
+                    # else if set_matrix[:, i] exist zero, but not all zero
+                    elif np.all(set_matrix[:, j]) == 0:
+                        # count how many zero in set_matrix[:, j]
+                        Psi1_sum += task_num - np.sum(set_matrix[:, j])
+                p_si_matrix[s, i] = (active_num - B_neq_sum - Psi1_sum) * all_gradients[s, i] / np.sum(all_gradients * set_matrix)
+    # update ineq_matrix and evaluation_matrix
+    ineq_matrix[0] = np.sum(p_si_matrix, axis=0)
+    ineq_matrix[1:] = p_si_matrix
+    evaluation_matrix[0] = ineq_matrix[0] <= (ki+1e-7)
+    evaluation_matrix[1:] = evaluation_matrix[1:] = ineq_matrix[1:] <= 1
+    return p_si_matrix, ineq_matrix, evaluation_matrix
+
+
+def optimal_sampling2(client_num, task_num, all_gradients, active_num, ki):
+    ineq_matrix = np.zeros((task_num+1, client_num))  # use the first one to store \sum<ki
+    evaluation_matrix = np.ones((task_num+1, client_num))
+    p_si_matrix = np.zeros((task_num, client_num))
+    set_matrix = np.ones((task_num, client_num))
+    # intialize: assume all p_si not breaks the rule
+    # p_si_matrix[s][i] = active_num * all_gradients[s][i] / np.sum(all_gradients)
+    p_si_matrix = active_num * np.array(all_gradients) / np.sum(all_gradients)
+    # update ineq_matrix
+    ineq_matrix[0] = np.sum(p_si_matrix, axis=0)
+    ineq_matrix[1:] = p_si_matrix
+    # update set_matrix
+    # if ineq_matrix[0][i] > ki, set_matrix[0][i] = 0
+    # for s=[1:], if ineq_matrix[s][i] > 1, set_matrix[s][i] = 0
+    evaluation_matrix[0] = ineq_matrix[0] <= ki
+    evaluation_matrix[1:] = ineq_matrix[1:] <= 1
+    # easiest case: set_matrix[0,i]==0, and np.all(set_matrix[1:,i])==1
+    easiest_index = []
+    easiest_done = False
+    while easiest_done is False:
+        for i in range(client_num):
+            temp = evaluation_matrix[1:,i]
+            if evaluation_matrix[0][i] == 0 and np.all(temp) == 1:
+                p_si_matrix[:, i] = B_neq_Prob(all_gradients[:, i], ki[i], task_num)
+                set_matrix[:, i] = 0
+                easiest_index.append(i)
+                p_si_matrix, ineq_matrix, evaluation_matrix  = update_matrices(client_num, task_num, ki, active_num, all_gradients,
+                                         set_matrix, p_si_matrix, ineq_matrix, evaluation_matrix)
+        # if all easiest cases are solved, then break
+        cnt = 0
+        for i in range(client_num):
+            temp = evaluation_matrix[1:,i]
+            if evaluation_matrix[0][i] == 0 and np.all(temp) == 1:
+                # not finish
+                easiest_done = False
+                break
+            else:
+                cnt += 1
+        if cnt == client_num:
+            easiest_done = True
+    print('finish step 1, remove the easiest clients')
+    # separate p_si to esaiest and not
+    # record not-easiest index
+    not_easiest_index = []
+    for i in range(client_num):
+        if i not in easiest_index:
+            not_easiest_index.append(i)
+    p_si_matrix_easiest = p_si_matrix[:, easiest_index]
+    all_gradients_temp = np.delete(all_gradients, easiest_index, axis=1)
+    sum_easiest = np.sum(p_si_matrix_easiest)
+    active_num_temp = active_num - sum_easiest
+
+    print('start the step 2')
+    while np.all(evaluation_matrix) != 1:
+        p_s_i = get_optimal_sampling_tasks(m=active_num_temp, gradient_record=all_gradients_temp)
+        print("finish step 2.1, if not consider sum<ki, then we are good")
+        # recover p_si_matrix
+        p_si_matrix = np.zeros((task_num, client_num))
+        p_si_matrix[:, easiest_index] = p_si_matrix_easiest
+        p_si_matrix[:, not_easiest_index] = p_s_i
+        # update evaluation
+        ineq_matrix[0] = np.sum(p_si_matrix, axis=0)
+        ineq_matrix[1:] = p_si_matrix
+        evaluation_matrix[0] = ineq_matrix[0] <= ki+1e-7
+        evaluation_matrix[1:] = ineq_matrix[1:] <= 1
+
+
+        temp_evaluation2 = evaluation_matrix[0]
+        if np.all(temp_evaluation2) != 1:
+            gap = ineq_matrix[0] - ki
+            # find the index of the largest gap
+            i = np.argmax(gap)
+            p_si_matrix[:, i] = B_neq_Prob(all_gradients[:, i], ki[i], task_num)
+            set_matrix[:, i] = 0
+            easiest_index.append(i)
+            not_easiest_index.remove(i)
+            p_si_matrix_easiest = p_si_matrix[:, easiest_index]
+            all_gradients_temp = np.delete(all_gradients, easiest_index, axis=1)
+            sum_easiest = np.sum(p_si_matrix_easiest)
+            active_num_temp = active_num - sum_easiest
+            print('finish step 2.2, consider sum<ki')
+
+
+    # print('final evaluation_matrix\n', evaluation_matrix)
+    return p_si_matrix
 
 def get_optimal_sampling(chosen_clients, all_data_num, gradient_record, args): # gradient record is norm
     # gradient_record: the shape is [task_index][client_index]
@@ -399,7 +500,9 @@ def get_optimal_sampling_cvx(chosen_clients, clients_task, all_data_num, gradien
 
     # p_optimal = optimal_solver(client_num=all_clients_num, task_num=tasks_num, all_gradients=all_gradients, ms_list=ms_list)
     #p_optimal = tradeoff_solver(client_num=all_clients_num, task_num=tasks_num, all_gradients=all_gradients, active_num=sample_num, dis=d_is)
-    p_optimal = communication_solver(client_num=all_clients_num, task_num=tasks_num, all_gradients=all_gradients,
+    #p_optimal = communication_solver(client_num=all_clients_num, task_num=tasks_num, all_gradients=all_gradients,
+                                #active_num=sample_num, ki=client_task_ability)
+    p_optimal = optimal_sampling2(client_num=all_clients_num, task_num=tasks_num, all_gradients=all_gradients,
                                 active_num=sample_num, ki=client_task_ability)
     p_s_i = p_optimal
 
