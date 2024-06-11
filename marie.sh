c329a0a2f59042a85d6206e6ca2713ecfb53edf9
Haoran Zhang <haoranz5@andrew.cmu.edu>
2024-06-11 15:49:30 -0400

    adjust L

--- marie.sh
+++ marie.sh
@@ -10,15 +10,15 @@ active=1.0
 r=120
 clientnum=20
 for sd in "${seedlist[@]}"; do
-python main.py --L 100 --unbalance 0.0 0.1 --fairness taskfair --alpha 3 --notes mariepaper_qFel_a3_$sd --C $active --num_clients $clientnum --class_ratio 0.8 0.8 0.8 --iid_type noniid noniid noniid --task_type $task_list0 --algo_type proposed --seed $sd --cpumodel --local_epochs 5 5 5 5 5 --round_num $r --insist
+python main.py --L 1 --unbalance 0.0 0.1 --fairness taskfair --alpha 3 --notes mariepaper_qFel_a3_$sd --C $active --num_clients $clientnum --class_ratio 0.8 0.8 0.8 --iid_type noniid noniid noniid --task_type $task_list0 --algo_type proposed --seed $sd --cpumodel --local_epochs 5 5 5 5 5 --round_num $r --insist
 done
 
 for sd in "${seedlist[@]}"; do
-python main.py --L 100 --unbalance 0.0 0.1 --fairness taskfair --alpha 3 --notes mariepaper_qFel_a3_$sd --C $active --num_clients $clientnum --class_ratio 0.8 0.8 0.8 0.8 0.8 --iid_type noniid noniid noniid noniid noniid --task_type $task_list1 --algo_type proposed --seed $sd --cpumodel --local_epochs 5 5 5 5 5 --round_num $r --insist
+python main.py --L 1 --unbalance 0.0 0.1 --fairness taskfair --alpha 3 --notes mariepaper_qFel_a3_$sd --C $active --num_clients $clientnum --class_ratio 0.8 0.8 0.8 0.8 0.8 --iid_type noniid noniid noniid noniid noniid --task_type $task_list1 --algo_type proposed --seed $sd --cpumodel --local_epochs 5 5 5 5 5 --round_num $r --insist
 done
 
 for sd in "${seedlist[@]}"; do
-python main.py --L 100 --unbalance 0.0 0.1 --fairness taskfair --alpha 3 --notes mariepaper_qFel_a3_$sd --C $active --num_clients $clientnum --class_ratio 0.8 0.8 0.8 0.8 0.8 0.8 0.8 0.8 0.8 0.8 --iid_type noniid noniid noniid noniid noniid noniid noniid noniid noniid noniid --task_type $task_list2 --algo_type proposed --seed $sd --cpumodel --local_epochs 5 5 5 5 5 5 5 5 5 5 --round_num $r --insist
+python main.py --L 1 --unbalance 0.0 0.1 --fairness taskfair --alpha 3 --notes mariepaper_qFel_a3_$sd --C $active --num_clients $clientnum --class_ratio 0.8 0.8 0.8 0.8 0.8 0.8 0.8 0.8 0.8 0.8 --iid_type noniid noniid noniid noniid noniid noniid noniid noniid noniid noniid --task_type $task_list2 --algo_type proposed --seed $sd --cpumodel --local_epochs 5 5 5 5 5 5 5 5 5 5 --round_num $r --insist
 done
 
 task_list4="mnist cifar10 fashion_mnist emnist cifar10"
@@ -28,6 +28,6 @@ r=120
 d=0.5
 for clientnum in "${clientnumlist[@]}"; do
 for sd in "${seedlist[@]}"; do
-python main.py --L 100 --unbalance 0.0 0.1 --data_ratio $d --fairness taskfair --alpha 3 --notes mariepaper_exp3_qFel_a3_$sd --C $active --num_clients $clientnum --class_ratio 0.8 0.8 0.8 0.8 0.8 --iid_type noniid noniid noniid noniid noniid --task_type $task_list4 --algo_type proposed --seed $sd --cpumodel --local_epochs 5 5 5 5 5 --round_num $r --insist
+python main.py --L 1 --unbalance 0.0 0.1 --data_ratio $d --fairness taskfair --alpha 3 --notes mariepaper_exp3_qFel_a3_$sd --C $active --num_clients $clientnum --class_ratio 0.8 0.8 0.8 0.8 0.8 --iid_type noniid noniid noniid noniid noniid --task_type $task_list4 --algo_type proposed --seed $sd --cpumodel --local_epochs 5 5 5 5 5 --round_num $r --insist
 done
 done
