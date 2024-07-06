faffb6acd31217ad7aa8a81b49186210bae73169
Haoran Zhang <haoranz5@andrew.cmu.edu>
2024-07-05 23:31:13 -0400

    backup

--- experiment_tmux3.sh
+++ experiment_tmux3.sh
@@ -1,7 +1,7 @@
 seedlist=(14 15 16 17 18 19)
 a=1
 unbalance_value=(0.9)
-dlist=(0.3) # data ratio
+dlist=(0.5) # data ratio
 #C=(0.025 0.05 0.1 0.2) # active rate
 C=(0.1) # active rate
 task_idx="fashion_mnist fashion_mnist fashion_mnist fashion_mnist fashion_mnist"
@@ -13,11 +13,13 @@ for uv in "${unbalance_value[@]}"; do
   for d in "${dlist[@]}"; do
     for c in "${C[@]}"; do
     for sd in "${seedlist[@]}"; do
-#python main.py --L 1 --venn_list 0.9 0.1 0.0 --fairness notfair --data_ratio $d --unbalance $uv 0.1 --alpha $a --notes distribution_lessVenn_AS_a"$a"c"$c"u"$uv"_$sd --alpha_loss --approx_optimal --C $c --num_clients $client_n --class_ratio 0.3 0.3 0.3 0.3 0.3 --iid_type $iid --task_type $task_idx --algo_type proposed --seed $sd --cpumodel --local_epochs 5 5 5 5 5 --round_num 150 --insist
-#python main.py --L 1 --fresh_ratio 0.1 --freshness --venn_list 0.9 0.1 0.0 --fairness notfair --data_ratio $d --unbalance $uv 0.1 --alpha $a --notes distribution_lessVenn_ASF0.1_a"$a"c"$c"u"$uv"_$sd --alpha_loss --approx_optimal --C $c --num_clients $client_n --class_ratio 0.3 0.3 0.3 0.3 0.3 --iid_type $iid --task_type $task_idx --algo_type proposed --seed $sd --cpumodel --local_epochs 5 5 5 5 5 --round_num 150 --insist
-#python main.py --L 1 --fresh_ratio 0.05 --freshness --venn_list 0.9 0.1 0.0 --fairness notfair --data_ratio $d --unbalance $uv 0.1 --alpha $a --notes distribution_lessVenn_ASF0.05_a"$a"c"$c"u"$uv"_$sd --alpha_loss --approx_optimal --C $c --num_clients $client_n --class_ratio 0.3 0.3 0.3 0.3 0.3 --iid_type $iid --task_type $task_idx --algo_type proposed --seed $sd --cpumodel --local_epochs 5 5 5 5 5 --round_num 150 --insist
-#python main.py --L 1 --fresh_ratio 0.01 --freshness --venn_list 0.9 0.1 0.0 --fairness notfair --data_ratio $d --unbalance $uv 0.1 --alpha $a --notes distribution_lessVenn_ASF0.01_a"$a"c"$c"u"$uv"_$sd --alpha_loss --approx_optimal --C $c --num_clients $client_n --class_ratio 0.3 0.3 0.3 0.3 0.3 --iid_type $iid --task_type $task_idx --algo_type proposed --seed $sd --cpumodel --local_epochs 5 5 5 5 5 --round_num 150 --insist
-python main.py --L 1 --client_cpu 1.0 0 0 --fairness notfair --fullparticipation --data_ratio $d --unbalance $uv 0.1 --alpha $a --notes distribution_lessVenn_full_u"$uv"_$sd --C $c --num_clients $client_n --class_ratio 0.3 0.3 0.3 0.3 0.3 --iid_type $iid --task_type $task_idx --algo_type proposed --seed $sd --cpumodel --local_epochs 5 5 5 5 5 --round_num 150 --insist
+python main.py --L 1 --venn_list 0.9 0.1 0.0 --fairness notfair --data_ratio $d --unbalance $uv 0.05 --alpha $a --notes lessVennc"$c"uv"$uv"0.05_AS_$sd --alpha_loss --approx_optimal --C $c --num_clients $client_n --class_ratio 0.3 0.3 0.3 0.3 0.3 --iid_type $iid --task_type $task_idx --algo_type proposed --seed $sd --cpumodel --local_epochs 5 5 5 5 5 --round_num 150 --insist
+#python main.py --L 1 --fresh_ratio 0.1 --freshness --venn_list 0.9 0.1 0.0 --fairness notfair --data_ratio $d --unbalance $uv 0.1 --alpha $a --notes lessVenn_ASF0.1_a"$a"c"$c"u"$uv"_$sd --alpha_loss --approx_optimal --C $c --num_clients $client_n --class_ratio 0.3 0.3 0.3 0.3 0.3 --iid_type $iid --task_type $task_idx --algo_type proposed --seed $sd --cpumodel --local_epochs 5 5 5 5 5 --round_num 150 --insist
+#python main.py --L 1 --fresh_ratio 0.05 --freshness --venn_list 0.9 0.1 0.0 --fairness notfair --data_ratio $d --unbalance $uv 0.1 --alpha $a --notes lessVenn_ASF0.05_a"$a"c"$c"u"$uv"_$sd --alpha_loss --approx_optimal --C $c --num_clients $client_n --class_ratio 0.3 0.3 0.3 0.3 0.3 --iid_type $iid --task_type $task_idx --algo_type proposed --seed $sd --cpumodel --local_epochs 5 5 5 5 5 --round_num 150 --insist
+#python main.py --L 1 --fresh_ratio 0.01 --freshness --venn_list 0.9 0.1 0.0 --fairness notfair --data_ratio $d --unbalance $uv 0.1 --alpha $a --notes lessVenn_ASF0.01_a"$a"c"$c"u"$uv"_$sd --alpha_loss --approx_optimal --C $c --num_clients $client_n --class_ratio 0.3 0.3 0.3 0.3 0.3 --iid_type $iid --task_type $task_idx --algo_type proposed --seed $sd --cpumodel --local_epochs 5 5 5 5 5 --round_num 150 --insist
+#python main.py --L 1 --client_cpu 1.0 0 0 --fairness notfair --fullparticipation --data_ratio $d --unbalance $uv 0.05 --alpha $a --notes lessVennc"$c"uv"$uv"0.05_full_$sd --C $c --num_clients $client_n --class_ratio 0.3 0.3 0.3 0.3 0.3 --iid_type $iid --task_type $task_idx --algo_type proposed --seed $sd --cpumodel --local_epochs 5 5 5 5 5 --round_num 150 --insist
+python main.py --L 1 --venn_list 0.9 0.1 0.0 --fairness notfair --data_ratio $d --unbalance $uv 0.05 --alpha $a --notes lessVennc"$c"uv"$uv"0.05_random_$sd --C $c --num_clients $client_n --class_ratio 0.3 0.3 0.3 0.3 0.3 --iid_type $iid --task_type $task_idx --algo_type random --seed $sd --cpumodel --local_epochs 5 5 5 5 5 --round_num 150 --insist
+python main.py --L 1 --venn_list 0.9 0.1 0.0 --fairness notfair --data_ratio $d --unbalance $uv 0.05 --alpha $a --notes lessVennc"$c"uv"$uv"0.05_OS_$sd --alpha_loss --optimal_sampling --C $c --num_clients $client_n --class_ratio 0.3 0.3 0.3 0.3 0.3 --iid_type $iid --task_type $task_idx --algo_type proposed --seed $sd --cpumodel --local_epochs 5 5 5 5 5 --round_num 150 --insist
 # to record local loss value
 done
 done
